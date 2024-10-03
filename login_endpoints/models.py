4179e06bd8ffe03b578ef1f38d93189360595c39
owlin <owlin@gmail.com>
2024-10-03 14:23:27 -0300

    Se implemento un UUID en el archivo models.py:

--- login_endpoints/models.py
+++ login_endpoints/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django import forms
+import uuid
 
 class Login(forms.Form):
     gmail = forms.EmailField()
     contrasena = forms.CharField(max_length=30)
-
+    pk = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
