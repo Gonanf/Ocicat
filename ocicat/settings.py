d83b6ce8f55425f11b91450309566b4f55e4274b
Chaos <gabysolotorevsky100@gmail.com>
2024-10-22 11:29:50 -0300

    No se puede hacer lo de https ni lo de cookies

--- ocicat/settings.py
+++ ocicat/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/' 
 
+
+
 X_FRAME_OPTIONS = "SAMEORIGIN"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
