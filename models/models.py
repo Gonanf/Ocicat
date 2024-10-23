b076bb7034f5abb1fb5b67844047319794627187
Gabriel <gabysolotorevsky100@gmail.com>
2024-10-23 17:50:40 -0300

    post publication terminado

--- models/models.py
+++ models/models.py
@@ -11,20 +11,20 @@ class PUBLICACION(models.Model):
     fecha = models.DateField(default=datetime.date.today)
     archivos = models.ManyToManyField("MEDIA",related_name="files")
     portada = models.ForeignKey("MEDIA",on_delete=models.SET_NULL, null=True)
-    dv = models.IntegerField()
+    dv = models.IntegerField(default=0)
 
 class USUARIO(models.Model):
     nombre = models.CharField(max_length=50,default="PLACEHOLDER NAME")
     gmail = models.EmailField(default="PLACEHOLDER GMAIL")
     contrasena = models.CharField(max_length=30, default="PLACEHOLDER PASSWORD")
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    dv = models.IntegerField()
+    dv = models.IntegerField(default=0)
 
 class MEDIA(models.Model):
     archivo = models.FileField(upload_to="media/")
-    dv = models.IntegerField()
+    dv = models.IntegerField(default=0)
 
-    
+
 @receiver(models.signals.post_delete, sender=MEDIA)
 def on_delete(sender, instance, **kargs):
 
@@ -36,11 +36,8 @@ def on_delete(sender, instance, **kargs):
 
 class CATEGORIA(models.Model):
     nombre = models.CharField(max_length=50)
+    dv = models.IntegerField(default=0)
 
 class DIGITOS_VERIFICADORES(models.Model):
     tabla = models.CharField(max_length=50)
-    dv = models.IntegerField()
-    
-
-
-
+    dv = models.IntegerField(default=0)
