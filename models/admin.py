ae642ea7c8d85372ca5ee1e0c90fbd0ce58ee871
Gabriel <gabysolotorevsky100@gmail.com>
2024-10-26 19:36:21 -0300

    Added Publication and DV in the admin page

--- models/admin.py
+++ models/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
-from .models import PUBLICACION,USUARIO,MEDIA,CATEGORIA
+from .models import PUBLICACION,USUARIO,MEDIA,CATEGORIA,DIGITOS_VERIFICADORES
 # Register your models here.
 class USUARIO_ADMIN(admin.ModelAdmin):
     list_display = ['nombre']
 
 admin.site.register(USUARIO, USUARIO_ADMIN)
+
+
 @admin.register(MEDIA)
 class MEDIA_ADMIN(admin.ModelAdmin):
     list_display = ['archivo']
+
+
 @admin.register(CATEGORIA)
 class CATEGORIA_ADMIN(admin.ModelAdmin):
     list_display = ['nombre']
+
+@admin.register(PUBLICACION)
+class PUBLICACION_ADMIN(admin.ModelAdmin):
+    list_display = ['titulo']
+
+@admin.register(DIGITOS_VERIFICADORES)
+class DIGITOS_VERIFICADORES_ADMIN(admin.ModelAdmin):
+    list_display = ['tabla']
